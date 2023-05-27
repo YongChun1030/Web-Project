b74fb09b8a80c6e281905285a275b9778c9515ea
liyangxx <liyangchoong@gmail.com>
2023-05-27 15:43:07 +0800

    updated login page UI

--- index.php
+++ index.php
@@ -1,25 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Login</title>
-  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" href="style.css">
+  <title>Automated Birthday-Wisher</title>
 </head>
 <body>
-  <div class="container">
-    <h1>Login</h1>
-    <form action="login.php" method="post">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required>
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required>
-      <button type="submit">Login</button>
-    </form>
-    <?php
-    if (isset($_GET['error'])) {
-      $errorMessage = $_GET['error'];
-      echo '<p class="error">' . $errorMessage . '</p>';
-    }
-    ?>
-  </div>
+    <section>
+        <div class="form-box">
+            <div class="form-value">
+                <form action="login.php" method="post">
+                    <h2>Login</h2>
+                    <div class="inputbox">
+                        <ion-icon name="mail-outline"></ion-icon>
+                        <input type="text" name="username" required>
+                        <label for="">Email</label>
+                    </div>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline"></ion-icon>
+                        <input type="password" name="password" required>
+                        <label for="">Password</label>
+                    </div>
+                    <div class="remember">
+                        <label for=""><input type="checkbox" name="remember">Remember Me</label>
+                    </div>
+                    <button type="submit">Log in</button>                  
+                    
+                </form>
+                <?php
+                if (isset($_GET['error'])) {
+                  $errorMessage = $_GET['error'];
+                  echo '<p class="error">' . $errorMessage . '</p>';
+
+                  if ($errorMessage === 'You have been blocked.') {
+                    echo '<p class="error">Please contact the administrator for assistance.</p>';
+                  }
+                }
+                ?>
+            </div>
+            
+        </div>
+    </section>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
