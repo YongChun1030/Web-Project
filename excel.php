47a68182fc510c0f18da70602f00ef17f4cb49f7
chewjiaseng <chewjiaseng2866@gmail.com>
2023-06-07 23:49:41 +0800

    updated

---
+++ excel.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Welcome to excel data upload</p>
+    
+    <form action="main.php">
+        <p><input type="submit" value="Home"></p>
+    </form>
+</body>
+</html>
